077835937fa9b8a8b01f1ca3bbe61aa6ff94d14c
Kaoru Tachibana <toyship@gmail.com>
2023-04-03 19:55:38 +0900

    add new project

---
+++ Practice/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Practice
+//
+//  Created by Kaoru Tachibana on 2023/04/03.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
