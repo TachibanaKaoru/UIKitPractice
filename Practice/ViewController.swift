5aafcbc0aef8b3b5824cc71c38e106797c9fd9ef
Kaoru Tachibana <toyship@gmail.com>
2023-04-12 14:10:41 +0900

    add menu item

--- Practice/ViewController.swift
+++ Practice/ViewController.swift
@@ -7,10 +7,23 @@
 
 import UIKit
 
+struct MenuItem {
+    let title: String
+    let detail: String
+    let itemFunction: () -> Void
+}
+
+struct MenuItemGroup {
+    let title: String
+    let groupItem: [MenuItem]
+}
+
 class ViewController: UIViewController {
 
-    private let cellIdentifier: String = "mycell"
+    private let cellIdentifier: String = "menucell"
     private let tableView = UITableView(frame: .zero, style: .insetGrouped)
+
+    private var menuItems: [MenuItemGroup] = []
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -19,6 +32,8 @@ class ViewController: UIViewController {
 
         //背景をtableviewとあわせて目立たないようにする
         view.backgroundColor = UIColor.groupTableViewBackground
+
+        prepareMenuItems()
 
         tableView.dataSource = self
         tableView.delegate = self
@@ -32,20 +47,113 @@ class ViewController: UIViewController {
 
     }
 
+    private func prepareMenuItems() {
+
+        let menuA1 = MenuItem(
+            title: "Fruit",
+            detail: "DiffableDataSource",
+            itemFunction: {
+                print("A 1")
+                let fruits = FruitsCollectionViewController()
+
+                self.navigationController?.pushViewController(fruits, animated: true)
+            }
+        )
+
+        let menuA2 = MenuItem(title: "Fruit", detail: "aa", itemFunction: {
+            print("A 2")
+        })
+
+        let menuA3 = MenuItem(title: "A 3", detail: "aaa", itemFunction: {
+            print("A 3")
+        })
+
+        let menuA4 = MenuItem(title: "A 4", detail: "a", itemFunction: {
+            print("A 4")
+        })
+
+        let menuA5 = MenuItem(title: "A 5", detail: "aaaaa", itemFunction: {
+            print("A 5")
+        })
+
+        let menuB1 = MenuItem(title: "B 1", detail: "bbb", itemFunction: {
+            print("B 1")
+        })
+
+        let menuB2 = MenuItem(title: "B 2", detail: "bc", itemFunction: {
+            print("B 2")
+        })
+
+        let menuC1 = MenuItem(title: "C 1", detail: "c", itemFunction: {
+            print("C 1")
+        })
+
+        let menuC2 = MenuItem(title: "C 2", detail: "ccc", itemFunction: {
+            print("C 2")
+        })
+
+        let menuC3 = MenuItem(title: "C 3", detail: "ccccc", itemFunction: {
+            print("C 3")
+        })
+
+        let menuA = MenuItemGroup(title: "UICollectionView", groupItem: [menuA1,menuA2,menuA3,menuA4,menuA5])
+        let menuB = MenuItemGroup(title: "UITableView", groupItem: [menuB1,menuB2])
+        let menuC = MenuItemGroup(title: "UIImageView", groupItem: [menuC1,menuC2,menuC3])
+
+        menuItems = [menuA,menuB,menuC]
+    }
+
 }
 
 extension ViewController: UITableViewDelegate {
 
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        print("selected \(indexPath)")
+
+        let targetMenuGroup: MenuItemGroup = menuItems[indexPath.section]
+        let targetMenuItem: MenuItem = targetMenuGroup.groupItem[indexPath.row]
+
+        targetMenuItem.itemFunction()
+
+        //switchで処理してくださいねー。
+        //最初は、differable data souceをしたかったのです。
+
+//        let fruits = FruitsCollectionViewController()
+//
+//        self.navigationController?.pushViewController(fruits, animated: true)
+    }
+
 }
 
 extension ViewController: UITableViewDataSource {
+
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return menuItems.count
+    }
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 10
+
+        let targetMenuGroup: MenuItemGroup = menuItems[section]
+
+        return targetMenuGroup.groupItem.count
+    }
+
+    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+
+        let targetMenuGroup: MenuItemGroup = menuItems[section]
+
+        return targetMenuGroup.title
     }
 
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
 
-        let cell = tableView.dequeueReusableCell(withIdentifier: cellIdentifier, for: indexPath)
+        let cell = UITableViewCell(style: .subtitle, reuseIdentifier: cellIdentifier)
+
+        let targetMenuGroup: MenuItemGroup = menuItems[indexPath.section]
+        let targetMenuItem: MenuItem = targetMenuGroup.groupItem[indexPath.row]
+
+        cell.textLabel?.text = targetMenuItem.title
+        cell.detailTextLabel?.text = targetMenuItem.detail
+
         return cell
     }
 }
